b01340baa432a73a7d698c678782befdf12471a1
Nilesh P <emailnileshp@gmail.com>
2019-03-13 00:53:07 -0400

    updated

--- src/MLApp.js
+++ src/MLApp.js
@@ -8,6 +8,9 @@ class MLApp extends Component {
 
   constructor() {
     super();
+    this.audioSource = React.createRef();
+    this.audioOutput = React.createRef();
+    this.videoSource = React.createRef();
     this.webcamElement = React.createRef();  
     this.results = React.createRef();
     this.classifier = knnClassifier.create();
@@ -20,10 +23,10 @@ class MLApp extends Component {
 
   componentDidMount() {
     document.title = "ML Classifier App"
-    Promise.all([this.loadModel(),this.setupWebcam()]).then(_ => this.runClassifier());
+    Promise.all([this.loadMLModel(),this.setupWebcam()]).then(_ => this.runClassifier());
   }
 
-  async loadModel() {
+  async loadMLModel() {
     return new Promise(async (resolve, reject) => {
       console.log('Loading mobilenet..');
       try {
@@ -46,7 +49,9 @@ class MLApp extends Component {
       navigator.getUserMedia = navigator.getUserMedia ||
           navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
           navigatorAny.msGetUserMedia;
-        if (navigator.getUserMedia) {
+
+
+          if (navigator.getUserMedia) {
         navigator.permissions.query({name:'camera'}).then(result => {
           if(result.state !== 'granted') {
             this.setState({ 
@@ -60,8 +65,10 @@ class MLApp extends Component {
               cameraLoading: '' 
             }, () => {
               //Object.assign(this.webcamElement.current, {srcObject:stream}, {'onloadeddata':() => resolve()})
-              this.webcamElement.current.srcObject = stream;
-              this.webcamElement.current.addEventListener('loadeddata',  () => resolve(), false);
+              const videoElement = this.webcamElement.current;
+              videoElement.srcObject = stream;
+              videoElement.addEventListener('loadeddata',  () => resolve(), false);
+              this.loadMediaOptions();
               console.log('cam setup complete!!');
               return resolve();
             });               
@@ -82,6 +89,109 @@ class MLApp extends Component {
         );               
       }
     });
+  }
+
+  async loadMediaOptions() {
+    const videoElement = this.webcamElement.current;// document.querySelector('video');
+    const audioInputSelect = this.audioSource.current;// document.querySelector('select#audioSource');
+    const audioOutputSelect = this.audioOutput.current;// document.querySelector('select#audioOutput');
+    const videoSelect = this.videoSource.current;// document.querySelector('select#videoSource');
+    const selectors = [audioInputSelect, audioOutputSelect, videoSelect];
+    
+    audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
+    
+    function gotDevices(deviceInfos) {
+      // Handles being called several times to update labels. Preserve values.
+      const values = selectors.map(select => select.value);
+      selectors.forEach(select => {
+        while (select.firstChild) {
+          select.removeChild(select.firstChild);
+        }
+      });
+      for (let i = 0; i !== deviceInfos.length; ++i) {
+        const deviceInfo = deviceInfos[i];
+        const option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === 'audioinput') {
+          option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
+          audioInputSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'audiooutput') {
+          option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`;
+          audioOutputSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+          option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+          videoSelect.appendChild(option);
+        } else {
+          console.log('Some other kind of source/device: ', deviceInfo);
+        }
+      }
+      selectors.forEach((select, selectorIndex) => {
+        if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
+          select.value = values[selectorIndex];
+        }
+      });
+    }
+    
+    navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+    
+    // Attach audio output device to video element using device/sink ID.
+    function attachSinkId(element, sinkId) {
+      if (typeof element.sinkId !== 'undefined') {
+        element.setSinkId(sinkId)
+          .then(() => {
+            console.log(`Success, audio output device attached: ${sinkId}`);
+          })
+          .catch(error => {
+            let errorMessage = error;
+            if (error.name === 'SecurityError') {
+              errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`;
+            }
+            console.error(errorMessage);
+            // Jump back to first output device in the list as it's the default.
+            audioOutputSelect.selectedIndex = 0;
+          });
+      } else {
+        console.warn('Browser does not support output device selection.');
+      }
+    }
+    
+    function changeAudioDestination() {
+      const audioDestination = audioOutputSelect.value;
+      attachSinkId(videoElement, audioDestination);
+    }
+    
+    function gotStream(stream) {
+      window.stream = stream; // make stream available to console
+      videoElement.srcObject = stream;
+      // Refresh button list in case labels have become available
+      return navigator.mediaDevices.enumerateDevices();
+    }
+    
+    function handleError(error) {
+      console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+    }
+    
+    function start() {
+      if (window.stream) {
+        window.stream.getTracks().forEach(track => {
+          track.stop();
+        });
+      }
+      const audioSource = audioInputSelect.value;
+      const videoSource = videoSelect.value;
+      const constraints = {
+        audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
+        video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+      };
+      navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
+    }
+    
+    audioInputSelect.onchange = start;
+    audioOutputSelect.onchange = changeAudioDestination;
+    
+    videoSelect.onchange = start;
+    
+    start();
   }
 
   // Reads an image from the webcam and associates it with a specific class index.
@@ -129,8 +239,21 @@ class MLApp extends Component {
             (
               <div className="workingContainer">
                 <h2>Machine learning for image classification</h2>
+                <div className="optionsContainer">
+                  <div className="select" hidden>
+                      <label htmlFor="audioSource">Audio input source: </label><select id="audioSource" ref={this.audioSource}></select>
+                  </div>
+
+                  <div className="select" hidden>
+                      <label htmlFor="audioOutput">Audio output destination: </label><select id="audioOutput" ref={this.audioOutput} ></select>
+                  </div>
+
+                  <div className="select">
+                      <label htmlFor="videoSource">Video source: </label><select id="videoSource" ref={this.videoSource} ></select>
+                  </div>
+                </div>
                 <div>
-                  <video autoPlay playsInline muted width="80%" height="80%" ref={this.webcamElement} ></video>
+                    <video autoPlay playsInline muted width="80%" height="80%" ref={this.webcamElement} ></video>
                 </div>
                 <div>
                   (for best experience try in chrome browser)
